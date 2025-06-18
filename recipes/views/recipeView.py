514d379ca2963eea0067bcff59fc8de45b0f3805
FrostyValue <saturmendezc@gmail.com>
2025-06-18 13:31:21 +0200

    fix(CFG2-SMC): Deleted depuration lines.

--- recipes/views/recipeView.py
+++ recipes/views/recipeView.py
@@ -53,31 +53,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         user = request.user
         count = int(request.query_params.get('count', 5))
 
-        print(f"\n--- Inicio de la acción 'random' ---")
-        print(f"1. Usuario autenticado (ID): {user.id}")
-        print(f"1. Usuario autenticado (Username): {user.username}")
-
         favorited_recipe_ids = Favorite.objects.filter(user_id=user).values_list('recipe_id', flat=True)
-        print(f"2. IDs de recetas favoritas del usuario {user.id}: {list(favorited_recipe_ids)}")
-
         available_recipes_qs = Recipe.objects.exclude(id__in=favorited_recipe_ids).prefetch_related('categories')
 
         available_recipe_ids = list(available_recipes_qs.values_list('id', flat=True))
-        print(f"4. IDs de recetas DISPONIBLES (NO FAVORITAS) para el usuario {user.id}: {available_recipe_ids}")
 
         if not available_recipe_ids:
-            print("5. No hay recetas no favoritas disponibles. Devolviendo lista vacía.")
             return Response([])
 
         num_to_select = min(count, len(available_recipe_ids))
         random_ids = random.sample(available_recipe_ids, num_to_select)
-        print(f"5. Recetas seleccionadas aleatoriamente (de las no favoritas): {random_ids}")
 
         random_recipes = available_recipes_qs.filter(id__in=random_ids)
         serializer = RandomRecipePublicSerializer(random_recipes, many=True)
-
-
-        print(f"6. Datos finales de recetas enviados al frontend: {serializer.data}")
-        print(f"--- Fin de la acción 'random' ---\n")
 
         return Response(serializer.data)
