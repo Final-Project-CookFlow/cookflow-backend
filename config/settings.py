a57d8db6502e59bb2692ae341b9fa0bcc2d6c656
FrostyValue <saturmendezc@gmail.com>
2025-06-18 15:00:57 +0200

    Fix(CFG2): settings.py fix.

--- config/settings.py
+++ config/settings.py
@@ -60,10 +60,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    #Personal test purposes SMC
-    #"http://192.168.1.104:5173",
+   "http://localhost:5173",
+   "http://127.0.0.1:5173",
 ]
 
 #CORS_ALLOW_ALL_ORIGINS = True
